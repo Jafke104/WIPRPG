e484cfe3a36037407a049cf48e6ec25a68cb4155
Jafke104 <eddevera@uci.edu>
2018-12-23 16:01:19 -0800

    Animation testing in EtthanScene

---
+++ RPG/Assets/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    public Animator animator;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("hSpeed", Mathf.Abs(horizontalMove));
+        gameObject.transform.GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalMove * Time.fixedDeltaTime, 0);
+		
+	}
+}
